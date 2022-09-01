9d40dd18448e12701c7040042aedacfbb86cdf56
leozizz <leonardocps2015@gmail.com>
2022-08-31 21:15:00 -0300

    Inserting script structure

---
+++ assets/script/script.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll(".slide")
+const backButton = document.getElementById("back-button")
+const nextButton = document.getElementById("next-button")
+
+let actualImage = 0
+
+nextButton.addEventListener("click", function () {
+    
+    if (actualImage === images.length -1){
+        return
+    }
+
+    hideImage()
+
+    actualImage++
+
+    images[actualImage].classList.add("show")
+
+    showOrHideButtons()
+})
+
+backButton.addEventListener("click", function () {
+    
+    if (actualImage === 0){
+        return
+    }
+
+    hideImage()
+    
+    actualImage--
+
+    images[actualImage].classList.add("show")
+
+    showOrHideButtons()
+})
+
+function hideImage() {
+    const openedImage = document.querySelector(".show")
+    openedImage.classList.remove("show")
+}
+
+function showOrHideButtons() {
+    const notTheFirstImage = actualImage !== 0
+    if (notTheFirstImage) {
+        backButton.classList.remove("opacity")
+    } else {
+        backButton.classList.add("opacity")
+    }
+
+    const thatIsTheLastImage = actualImage !== 0 && actualImage == images.length - 1
+    if (thatIsTheLastImage) {
+        nextButton.classList.add("opacity")
+    } else {
+        nextButton.classList.remove("opacity")
+    }
+}
