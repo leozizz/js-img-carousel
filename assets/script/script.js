fe5875bab7163ecc5c9fdc4a5d61c36b683d9e9b
leozizz <leonardocps2015@gmail.com>
2022-08-31 21:27:56 -0300

    adding some comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,12 +4,12 @@ const nextButton = document.getElementById("next-button")
 
 let actualImage = 0
 
+//listen for the 'click' event on nextButton and execute the functions responsible for passing the images
 nextButton.addEventListener("click", function () {
     
     if (actualImage === images.length -1){
         return
     }
-
 
     actualImage++
 
@@ -18,6 +18,7 @@ nextButton.addEventListener("click", function () {
     showOrHideButtons()
 })
 
+//listen for the 'click' event on backButton and execute the functions responsible for passing the images
 backButton.addEventListener("click", function () {
     
     if (actualImage === 0){
@@ -31,15 +32,20 @@ backButton.addEventListener("click", function () {
     showOrHideButtons()
 })
 
+//add the "show" class to the current index element of images
 function showImage() {
     images[actualImage].classList.add("show")
 }
 
+//queries the elements with the "show" class and does the removal to hide the image
 function hideImage() {
     const openedImage = document.querySelector(".show")
     openedImage.classList.remove("show")
 }
 
+//checks the index of the current image
+//if it is greater than 0 it will remove the "opacity" class from the backButton
+//if same length as images, add "opacity" class to nextButton
 function showOrHideButtons() {
     const notTheFirstImage = actualImage !== 0
     if (notTheFirstImage) {
